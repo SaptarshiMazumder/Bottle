74288af1eca2afdb4208987098d579420ae899b9
Saptarshi Mazumder <48704857+SaptarshiMazumder@users.noreply.github.com>
2023-03-19 19:43:19 +0900

    Stories working to an extent

--- PostIT/static/v.1.01/js/swipe-stories.js
+++ PostIT/static/v.1.01/js/swipe-stories.js
@@ -1,42 +1,12 @@
-var swiper = new Swiper(".slide-content", {
-    slidesPerView: 8,
-    spaceBetween: 5,
-    loop: true,
-    centerSlide: 'true',
-    fade: 'true',
-    grabCursor: 'true',
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-
-    breakpoints:{
-        0: {
-            slidesPerView: 1,
-        },
-        400: {
-            slidesPerView: 2,
-        },
-        700: {
-            slidesPerView: 3,
-        },
-        900: {
-            slidesPerView: 4,
-        },
-        1100: {
-            slidesPerView: 5,
-        },
-        1300: {
-            slidesPerView: 6,
-        },
-        1920: {
-            slidesPerView: 8,
-        },
-    },
-  });
- 
+var mySwiper = new Swiper ('.slide-content', {
+  autoplay: false,
+  spaceBetween: 1,
+  slidesPerView: 'auto',
+  loop: true,
+  // If we need pagination
+  pagination: '.swiper-pagination',
+  
+  // Navigation arrows
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+});
