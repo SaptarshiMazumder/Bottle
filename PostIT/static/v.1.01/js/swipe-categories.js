4c025414a8c996f2b788b2c16c5f55fb5832840f
Saptarshi Mazumder <48704857+SaptarshiMazumder@users.noreply.github.com>
2023-04-08 18:43:52 +0900

    Home page menus added

---
+++ PostIT/static/v.1.01/js/swipe-categories.js
@@ -0,0 +1,30 @@
+var swiper = new Swiper(".home-page-categories", {
+    autoplay: false,
+    mousewheel: {
+        releaseOnEdges: true,
+        sensitivity: 10,
+    },
+    spaceBetween: 10,
+    slidesPerView: 'auto',
+    loop: false,
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+const categories_container = document.querySelector(".home-page-categories");
+
+categories_container.addEventListener('wheel', (e) => {
+    e.preventDefault();
+    categories_container.scrollLeft += e.deltaY;
+    console.log("Scrolled mouse");
+})
