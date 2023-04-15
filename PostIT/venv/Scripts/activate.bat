a856a8ab48d88efe290e88219cb621d5fff1fb58
SaptarshiMazumder <sapmazumder@gmail.com>
2023-04-15 15:01:08 +0900

    dont commit to main

--- PostIT/venv/Scripts/activate.bat
+++ PostIT/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\googler\OneDrive\Desktop\PostIT-Merged-Git-Clone\PostIT-Merged\DjangoWebapp-ToMerge\DjangoWebapp-ToMerge\DjangoDjangoDjango - Improving\PostIT\venv
+set VIRTUAL_ENV=C:\Users\sapma\Desktop\Bottle Repos\Bottle\PostIT\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -25,7 +25,6 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(venv) 
 
 :END
 if defined _OLD_CODEPAGE (
